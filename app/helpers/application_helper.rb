0f0fbc06ba8eb8a5224efe3152dec6c443f62d84
Thanh Huynh <thanhjamin@yahoo.com>
2013-10-29 14:25:12 -0700

    modal signup and signin and application helper for devise

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,20 @@ module ApplicationHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def resource_class
+  devise_mapping.to
+end
 end
