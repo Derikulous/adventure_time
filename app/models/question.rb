d7ac0dab8bbf292ed28d5a05aa9e4220a9d81a73
Derik Strattan <derik.strattan@gmail.com>
2013-10-31 13:49:19 -0700

    ballzz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :test
-  has_many :answers, :dependent => :destroy
+  has_many :answers, dependent:  :destroy
   has_many :solutions
-  accepts_nested_attributes_for :answers, allow_destroy: true
+  accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
