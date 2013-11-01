06d5d8090dc6ef0d323f90369a3b0532f4e1507f
James Chuang <jameschuang13@gmail.com>
2013-11-01 11:16:41 -0700

    fix float to round to 2 decimals

--- app/models/test.rb
+++ app/models/test.rb
@@ -27,7 +27,7 @@ class Test < ActiveRecord::Base
           end
         end
       end
-      return ((score / questions.size.to_f) * 100).to_s + ' %'
+      return ((score / questions.size.to_f) * 100).round(2).to_s + ' %'
     end
   end
 end
