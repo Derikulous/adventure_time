d0dd347ccd395248d19653be970f98637d0887fe
James Chuang <jameschuang13@gmail.com>
2013-10-31 19:44:28 -0700

    refactor checking answer method in solutions model

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -3,4 +3,25 @@ class Solution < ActiveRecord::Base
   belongs_to :user
   belongs_to :report
   #accepts_nested_attributes_for :question
+
+  def check_answer(q)
+    binding.pry
+    if q == find_answer.to_s
+      self.correct = true
+      self.save
+    end
+  end
+
+  private
+
+  def find_answer
+    count = 1
+    self.question.answers.each do |answer|
+      if answer.correct
+        return count
+      else
+        count+=1
+      end
+    end
+  end
 end
