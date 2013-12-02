98876358a9ee034cbc871ee64e76ce9952b7b51f
Thanh Huynh <thanhjamin@yahoo.com>
2013-12-02 14:29:10 -0800

    add experience to exams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131202210216) do
+ActiveRecord::Schema.define(version: 20131202222632) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20131202210216) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "experience"
   end
 
   create_table "questions", force: true do |t|
