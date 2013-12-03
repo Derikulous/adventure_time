5d4dd1c3bad5df8a5bbe5775f00101ef8e8c85b2
Derik Strattan <derik.strattan@gmail.com>
2013-12-03 11:58:29 -0800

    Fixed edit and create exam tests.

--- test/features/exam_test/admin_exam_test.rb
+++ test/features/exam_test/admin_exam_test.rb
@@ -20,6 +20,7 @@ feature "admin exams for offerletter" do
     fill_in 'exam_name', with: 'This is an example exam'
     fill_in 'exam_questions_attributes_0_content', with: 'This is a sample question'
     fill_in 'exam_questions_attributes_0_answers_attributes_1_content', with: 'This is a sample answer'
+    fill_in 'exam_experience', with: '100'
 
     # click on create exam
     click_on 'Submit'
@@ -45,14 +46,18 @@ feature "admin exams for offerletter" do
 
     # inputs exams info
     fill_in 'exam_name', with: 'Changing This Exam'
+    fill_in 'exam_questions_attributes_0_content', with: 'Not the same question'
+    fill_in 'exam_questions_attributes_0_answers_attributes_1_content', with: 'A different answer'
+    fill_in 'exam_experience', with: '1'
 
     # click on create exam
     click_on 'Submit'
 
     # Success message
     page.text.must_include 'Exam was successfully updated'
-    page.text.must_include 'Changing This Exam'
-    page.text.wont_include 'Example Test'
+    page.text.must_include 'Not the same question'
+    page.text.wont_include 'This is an example exam'
+    page.text.wont_include '100'
 
   end
 
