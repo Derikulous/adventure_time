214af481bc028bbc8560555dd05c2c8786cdab36
Derik Strattan <derik.strattan@gmail.com>
2013-12-03 17:25:10 -0800

    Added migration, experience to exams

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131203181430) do
+ActiveRecord::Schema.define(version: 20131204012410) do
 
   create_table "answers", force: true do |t|
     t.integer  "question_id"
@@ -26,8 +26,8 @@ ActiveRecord::Schema.define(version: 20131203181430) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "experience"
     t.integer  "level"
+    t.integer  "experience"
   end
 
   create_table "questions", force: true do |t|
