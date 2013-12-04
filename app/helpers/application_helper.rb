59e8fad174442f14e14d97e6d25141ff2654c77e
James Chuang <jameschuang13@gmail.com>
2013-12-04 11:41:24 -0800

    fix gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,10 +8,10 @@ module ApplicationHelper
   end
 
   # Returns the Gravatar (http://gravatar.com/) for the given user.
-  def gravatar_for(user)
+  def gravatar_for(user, klass)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar circle-grav")
+    image_tag(gravatar_url, alt: user.name, class: klass)
   end
 
   def resource_name
