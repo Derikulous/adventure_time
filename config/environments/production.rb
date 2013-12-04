0f7ceda7496a572233ac4a3ebe48e1f459b9637d
Derik Strattan <derik.strattan@gmail.com>
2013-12-03 21:41:02 -0800

    new push without sql

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,11 @@ Offerletter::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+    config.action_controller.asset_host = Proc.new do |source, request|
+      scheme = request.ssl? ? "https" : "http"
+      "#{scheme}://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
+    end
+
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
