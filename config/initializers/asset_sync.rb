12abaf4ebc85aeaf901b17640f9d563f9fc49c49
James Chuang <jameschuang13@gmail.com>
2013-12-04 09:49:45 -0800

    remove asset sync

--- config/initializers/asset_sync.rb
+++
@@ -1,9 +0,0 @@
-if defined?(AssetSync)
-  AssetSync.configure do |config|
-    config.fog_provider = 'AWS'
-    config.fog_directory = ENV['FOG_DIRECTORY']
-
-    # Fail silently.  Useful for environments such as Heroku
-    config.fail_silently = true
-  end
-end
