21b556367937d9e3a62c04bff09b9bfcf080f759
James Chuang <jameschuang13@gmail.com>
2014-03-05 11:19:46 -0800

    fix issue where experience, life and level can be edit by user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :experience, :level, :life)
+    params.require(:user).permit(:name, :email)
   end
 
   def check_user
